91b1f47301432b57b0d8c681daca0639afbfd047
lazbek <jannnjan592@gmail.com>
2022-01-26 13:41:49 +0500

    lazbek

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let hpbtn = document.querySelectorAll('.nav__btn2')[0],
+    modal = document.querySelectorAll('.modal')[0],
+    header__title2 = document.querySelectorAll('.header__title2')[0],
+    closeBtn = document.querySelector('.close'),
+    btn__name = document.querySelectorAll('.contactform_name')[0]
+    contactForm = document.getElementsByClassName('contactform_name') [0],
+    message = document.getElementsByName('message')[0],
+    footerBtn = document.getElementsByClassName('footer__btn2') [0]
+
+
+
+
+
+
+function hover() {
+    header__title2.textContent = 'lazjon'
+}
+header__title2.addEventListener('mouseenter', hover)
+header__title2.addEventListener('mouseleave', function () {
+    header__title2.textContent = 'smm'
+})
+
+hpbtn.addEventListener('click', function () {
+    modal.style.display = 'block'
+})
+
+
+closeBtn.addEventListener('click', function () {
+    modal.style.display = 'none'
+})
+contactForm.addEventListener('input', function () {
+    message.value = `my name is ${contactForm.value} i want:`    
+})
+footerBtn.addEventListener('click', function(){
+    modal.style.display = 'block'
+})
+
+
+let age = prompt('yoshingiz nechida')
+
+
+if(age < 18){
+    alert('xali vohli')
+}else if (age > 80) {
+    alert('uji kech')
+}else{
+    alert('qonday endi')
+}
